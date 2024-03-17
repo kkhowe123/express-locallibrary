48fb7a3111eaf47a60b1078d266a971c80bebf21
keanu howe <keanuhowe3@gmail.comgit config --global user.name keanu>
2024-03-17 05:37:26 -0500

    remove rate limiter

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const compression = require("compression");
 const helmet = require("helmet");
 const app = express();
 
-// Set up rate limiter: maximum of twenty requests per minute
-const RateLimit = require("express-rate-limit");
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
-});
-// Apply rate limiter to all requests
-app.use(limiter);
+// // Set up rate limiter: maximum of twenty requests per minute
+// const RateLimit = require("express-rate-limit");
+// const limiter = RateLimit({
+//   windowMs: 1 * 60 * 1000, // 1 minute
+//   max: 20,
+// });
+// // Apply rate limiter to all requests
+// app.use(limiter);
 
 //set up mongoose connection 
 
